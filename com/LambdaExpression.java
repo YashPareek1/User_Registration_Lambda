1332acfa587228ad054b7792928233d234166980
YashPareek1 <yashpareekg4@gmail.com>
2021-05-17 16:05:39 +0530

    Lambda Function uc2

--- com/LambdaExpression.java
+++ com/LambdaExpression.java
@@ -16,9 +16,13 @@ public class LambdaExpression {
 
 		System.out.println("Enter First Name: ");
 		String firstName = s.next();
-        
-		Lamda checkString=(String value) -> Pattern.matches("^[A-Z a-z]{3,25}$",firstName);
-		System.out.println(checkString.eval("value"));
+        Lamda Name=(String value) -> Pattern.matches("^[A-Z a-z]{3,25}$",firstName);
+		System.out.println(Name.eval("value"));
+		
+		System.out.println("Enter Last Name: ");
+		String LastName = s.next();
+        Lamda Lastname=(String value) -> Pattern.matches("^[A-Z a-z]{3,25}$",LastName);
+		System.out.println(Lastname.eval("value"));
 	}
 
 }
